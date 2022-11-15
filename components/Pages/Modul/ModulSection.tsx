09bb7b6a7a126689bdb9744dcc4d0fe98850d92b
akrindev <akrinmin@gmail.com>
2022-11-15 08:54:33 +0700

    fix: pagination as components

--- components/Pages/Modul/ModulSection.tsx
+++ components/Pages/Modul/ModulSection.tsx
@@ -1,4 +1,4 @@
-import { IconChevronLeft, IconChevronRight } from "@tabler/icons";
+import Pagination from "../../Pagination/Pagination";
 import ModulGrid from "./ModulGrid";
 import ModulSectionSide from "./ModulSectionSide";
 
@@ -14,47 +14,7 @@ export default function ModulSection() {
             <ModulGrid />
           ))}
 
-          <div className='mt-3'>
-            <ul className='pagination '>
-              <li className='page-item disabled'>
-                <a className='page-link' href='#'>
-                  <IconChevronLeft />
-                  prev
-                </a>
-              </li>
-              <li className='page-item'>
-                <a className='page-link' href='#'>
-                  1
-                </a>
-              </li>
-              <li className='page-item active'>
-                <a className='page-link' href='#'>
-                  2
-                </a>
-              </li>
-              <li className='page-item'>
-                <a className='page-link' href='#'>
-                  3
-                </a>
-              </li>
-              <li className='page-item'>
-                <a className='page-link' href='#'>
-                  4
-                </a>
-              </li>
-              <li className='page-item'>
-                <a className='page-link' href='#'>
-                  5
-                </a>
-              </li>
-              <li className='page-item'>
-                <a className='page-link' href='#'>
-                  next
-                  <IconChevronRight />
-                </a>
-              </li>
-            </ul>
-          </div>
+          <Pagination />
         </div>
       </div>
     </div>
