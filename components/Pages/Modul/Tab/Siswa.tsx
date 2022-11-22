934d674e3539e353e715355321cdad4498a4206d
akrindev <akrinmin@gmail.com>
2022-11-22 13:29:18 +0700

    fix: students list

--- components/Pages/Modul/Tab/Siswa.tsx
+++ components/Pages/Modul/Tab/Siswa.tsx
@@ -1,3 +1,57 @@
 export default function Siswa() {
-  return <div>Anggota modul / siswa</div>;
+  const names: Array<{ name: string }> = [
+    {
+      name: "Nanang Hermanto",
+    },
+    {
+      name: "Syakirin Amin",
+    },
+    {
+      name: "Rio Aprianto",
+    },
+    {
+      name: "Muhimmatul Iffadah",
+    },
+    {
+      name: "Leni Pratiwi",
+    },
+  ];
+
+  return (
+    <div className='card'>
+      <div className='table-responsive'>
+        <table className='table table-vcenter card-table'>
+          <thead>
+            <tr>
+              <th>Nama</th>
+              <th>Tanggal Lahir</th>
+            </tr>
+          </thead>
+          <tbody>
+            {names.map(({ name }) => (
+              <tr>
+                <td>
+                  <div className='d-flex py-1 align-items-center'>
+                    <span className='avatar me-2'>SA</span>
+                    <div className='flex-fill'>
+                      <div className='font-weight-medium'>{name}</div>
+                      <div className='text-muted'>
+                        <span className='text-reset'>
+                          {name.replace(" ", ".").toLowerCase()}@smeducative.com
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <div>Pekalongan</div>
+                  <div className='text-muted'>22 Agustus 2001</div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
