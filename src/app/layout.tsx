d4219163dad29ea4433ba5352f30c71cd1227fce
akrindev <akrinmin@gmail.com>
2023-06-10 12:13:38 +0700

    fix: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,19 @@
-"use client";
-
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { SessionProvider } from "next-auth/react";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
+import { Metadata } from "next";
 
 export const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    default: "Smeduverse Orbit",
+    template: "%s | Smeduverse Orbit",
+  },
+  description: "Smeduverse Orbit",
+};
 
 export default function RootLayout({
   children,
@@ -15,15 +21,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en' suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <head />
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
           inter.className
-        )}>
-        <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-          <SessionProvider>{children}</SessionProvider>
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
         </ThemeProvider>
         <Toaster />
       </body>
