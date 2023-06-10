58504c26a5b86c666396ffbe45747e225ff1f61f
akrindev <akrinmin@gmail.com>
2023-06-10 08:27:35 +0700

    Refactor layout component and add user navigation

---
+++ src/app/(authenticated)/components/user-nav.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { CreditCard, LogOut, PlusCircle, Settings, User } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MD5 } from "crypto-js"; // import the MD5 function from the crypto-js library
+
+const email = "hi@syakirinamin.com";
+const hash = MD5(email.trim().toLowerCase()).toString(); // use the MD5 function to generate the hash and convert it to a string
+const avatarUrl = `https://www.gravatar.com/avatar/${hash}?s=200&d=identicon&r=pg`;
+
+// ...
+
+<AvatarImage src={avatarUrl} alt="@shadcn" />;
+export function UserNav() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+          <Avatar className="h-8 w-8">
+            <AvatarImage src={avatarUrl} alt="@syakirinam" />
+            <AvatarFallback>SA</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">Syakirin Amin</p>
+            <p className="text-xs leading-none text-muted-foreground">
+              hi@syakirinamin.com
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <User className="mr-2 h-4 w-4" />
+            <span>Profile</span>
+            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Log out</span>
+          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
