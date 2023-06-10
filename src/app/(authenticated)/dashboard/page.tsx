dc14340737469590f62e2063b9f68a00eac36593
akrindev <akrinmin@gmail.com>
2023-06-10 12:46:06 +0700

    fix: add dashboard card count

--- src/app/(authenticated)/dashboard/page.tsx
+++ src/app/(authenticated)/dashboard/page.tsx
@@ -1,57 +1,64 @@
 "use client";
 
-import { Separator } from "@/components/ui/separator";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 
 export default function Page() {
+  const analitycs: Array<{
+    name: string;
+    value: number;
+  }> = [
+    {
+      name: "Mata Pelajaran",
+      value: 721,
+    },
+    {
+      name: "Modul",
+      value: 52,
+    },
+    {
+      name: "Materi",
+      value: 4573,
+    },
+    {
+      name: "Presensi",
+      value: 2234,
+    },
+  ];
+
   return (
-    <>
-      <Tabs defaultValue="music" className="h-full space-y-6">
-        <div className="space-between flex items-center">
-          <TabsList>
-            <TabsTrigger value="music" className="relative">
-              Music
-            </TabsTrigger>
-            <TabsTrigger value="podcasts">Podcasts</TabsTrigger>
-            <TabsTrigger value="live" disabled>
-              Live
-            </TabsTrigger>
-          </TabsList>
+    <div className="h-full flex flex-col space-y-5">
+      <div className="flex flex-col h-full">
+        <div className="flex items-center justify-between">
+          <div className="space-y-1">
+            <h2 className="text-2xl font-semibold tracking-tight">Dashboard</h2>
+            <p className="text-sm text-muted-foreground">
+              Selamat datang kembali
+            </p>
+          </div>
         </div>
-        <TabsContent value="music" className="border-none p-0 outline-none">
-          <div className="flex items-center justify-between">
-            <div className="space-y-1">
-              <h2 className="text-2xl font-semibold tracking-tight">
-                Listen Now
-              </h2>
-              <p className="text-sm text-muted-foreground">
-                Top picks for you. Updated daily.
-              </p>
-            </div>
-          </div>
-          <Separator className="my-4" />
-          <div className="relative">
-            <ScrollArea>Listen</ScrollArea>
-          </div>
-        </TabsContent>
-        <TabsContent
-          value="podcasts"
-          className="h-full flex-col border-none p-0 data-[state=active]:flex"
-        >
-          <div className="flex items-center justify-between">
-            <div className="space-y-1">
-              <h2 className="text-2xl font-semibold tracking-tight">
-                New Episodes
-              </h2>
-              <p className="text-sm text-muted-foreground">
-                Your favorite podcasts. Updated daily.
-              </p>
-            </div>
-          </div>
-          <Separator className="my-4" />
-        </TabsContent>
-      </Tabs>
-    </>
+
+        <div className="mt-5 grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+          {analitycs.map((item) => (
+            <Card
+              key={item.name}
+              className="shadow-md hover:scale-105 duration-500 cursor-pointer"
+            >
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">
+                  {item.name}
+                </CardTitle>
+                {/* <DollarSign className="h-4 w-4 text-muted-foreground" /> */}
+              </CardHeader>
+              <CardContent>
+                <div className="text-2xl font-bold">{item.value}</div>
+                {/* <p className="text-xs text-muted-foreground">
+                  jumlah keseluruhan mapel
+                </p> */}
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
