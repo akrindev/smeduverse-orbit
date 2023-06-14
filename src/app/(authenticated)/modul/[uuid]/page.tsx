121a96eccdcd9779fc4366294ea745eed8406054
akrindev <akrinmin@gmail.com>
2023-06-14 10:54:55 +0700

    fix: update modul card

---
+++ src/app/(authenticated)/modul/[uuid]/page.tsx
@@ -0,0 +1,3 @@
+export default function ModulPage({ params }: { params: { uuid: string } }) {
+  return <div>modul {params.uuid}</div>;
+}
--- src/app/(authenticated)/modul/page.tsx
+++ src/app/(authenticated)/modul/page.tsx
@@ -2,6 +2,7 @@ import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
 import ModulCard from "../components/ModulCard";
 import { Metadata } from "next";
+import { Modul } from "@/types/modul";
 
 export const metadata: Metadata = {
   title: "Modul",
@@ -9,83 +10,64 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
-  // yourModul dummy data has key owner, cover, class, name mapel, status
-  // cover is image url from random unsplash image
-  // status must be boolean
-  // owner is teacher name in this example there was 3 teacher
-  // mapel is matematika, fisika, kimia, biologi, bahasa indonesia, bahasa inggris, pkn, sejarah, geografi, ekonomi, sosiologi
-  const yourModul = [
+  // make dummy data for modul
+  const yourModul: Modul[] = [
     {
-      id: Math.random().toString(36),
-      owner: "Muhammad Fauzan",
-      cover: "https://picsum.photos/seed/picsum/500/500",
-      class: "XII IPA 2",
-      mapel: "Matematika",
-      status: true,
-    },
-    {
-      id: Math.random().toString(36),
-      owner: "Ahmad Syahrul",
-      cover: "https://picsum.photos/seed/picsum/500/500",
-      class: "XII IPA 2",
-      mapel: "Fisika",
-      status: true,
-    },
-    {
-      id: Math.random().toString(36),
-      owner: "Rizki Pratama",
-      cover: "https://picsum.photos/seed/picsum/500/500",
-      class: "XII IPA 2",
-      mapel: "Kimia",
-      status: true,
-    },
-    {
-      id: Math.random().toString(36),
-      owner: "Muhammad Fauzan",
-      cover: "https://picsum.photos/seed/picsum/500/500",
-      class: "XII IPA 2",
-      mapel: "Biologi",
-      status: true,
-    },
-    {
-      id: Math.random().toString(36),
-      owner: "Ahmad Syahrul",
-      cover: "https://picsum.photos/seed/picsum/500/500",
-      class: "XII IPA 2",
-      mapel: "Bahasa Indonesia",
-      status: true,
-    },
-    {
-      id: Math.random().toString(36),
-      owner: "Rizki Pratama",
-      cover: "https://picsum.photos/seed/picsum/500/500",
-      class: "XII IPA 2",
-      mapel: "Bahasa Inggris",
-      status: true,
-    },
-    {
-      id: Math.random().toString(36),
-      owner: "Muhammad Fauzan",
-      cover: "https://picsum.photos/seed/picsum/500/500",
-      class: "XII IPA 2",
-      mapel: "PKN",
-      status: true,
-    },
-    {
-      id: Math.random().toString(36),
-      owner: "Ahmad Syahrul",
-      cover: "https://picsum.photos/seed/picsum/500/500",
-      class: "XII IPA 2",
-      mapel: "Sejarah",
-      status: true,
-    },
-    {
-      id: Math.random().toString(36),
-      owner: "Rizki Pratama",
-      cover: "https://picsum.photos/seed/picsum/500/500",
-      class: "XII IPS 1",
-      mapel: "Geografi",
-      status: true,
+      uuid: "e0c6e072-650c-4ba4-b550-14a4cc15802b",
+      teacher_id: "00446645-ad22-4c8b-9b2b-ba0df6706ed2",
+      rombongan_belajar_id: "0890666e-df8b-4a03-bd80-03db3cb48928",
+      mapel_id: 1,
+      semester_id: 3,
+      status: 1,
+      created_at: "2023-05-22T03:36:55.000000Z",
+      updated_at: "2023-05-22T03:36:55.000000Z",
+      deleted_at: null,
+      teacher: {
+        teacher_id: "00446645-ad22-4c8b-9b2b-ba0df6706ed2",
+        fullname: "Rio aprianto",
+        niy: "2022201",
+      },
+      rombel: {
+        id: "0890666e-df8b-4a03-bd80-03db3cb48928",
+        nama: "XII-2 TBSM",
+        tingkat_kelas: 12,
+        jurusan_id: 3,
+        tahun_ajaran_id: 2,
+        wali_id: "02fda90f-fec1-4b70-a2db-49ed0a4fd2e8",
+        image: "/dist/images/saly-24.png",
+        status_aktif: 1,
+        created_at: "2022-08-02T05:31:20.000000Z",
+        updated_at: "2022-08-02T05:31:20.000000Z",
+        jurusan: {
+          id: 3,
+          nama: "Teknik dan Bisnis Sepeda Motor",
+          kode: "TBSM",
+          created_at: "2021-08-24T02:08:13.000000Z",
+          updated_at: "2021-08-24T02:08:13.000000Z",
+        },
+        wali_kelas: {
+          teacher_id: "02fda90f-fec1-4b70-a2db-49ed0a4fd2e8",
+          fullname: "I INTRI ATI, S.Pd",
+          niy: "2019206",
+        },
+      },
+      mapel: {
+        id: 1,
+        kode: "MTK",
+        nama: "Matematika",
+        created_at: "2021-09-07T05:26:25.000000Z",
+        updated_at: "2021-09-07T05:26:25.000000Z",
+        deleted_at: null,
+      },
+      semester: {
+        id: 3,
+        name: "2022/2021-1",
+        is_active: 1,
+        date_start: null,
+        date_end: null,
+        created_at: "2023-05-22T01:45:51.000000Z",
+        updated_at: "2023-05-22T01:45:51.000000Z",
+      },
     },
   ];
 
@@ -105,7 +87,7 @@ export default async function Page() {
             <div className="grid grid-cols-12 gap-5">
               {yourModul.map((modul) => (
                 <ModulCard
-                  key={modul.mapel}
+                  key={modul.uuid}
                   modul={modul}
                   className="cursor-pointer"
                 />
