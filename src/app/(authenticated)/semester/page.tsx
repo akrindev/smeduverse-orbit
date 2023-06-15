c55523a66ef86506071fe6d570371fff8b6cd252
akrindev <akrinmin@gmail.com>
2023-06-15 22:54:46 +0700

    fix: add alert info about semester

--- src/app/(authenticated)/semester/page.tsx
+++ src/app/(authenticated)/semester/page.tsx
@@ -1,5 +1,7 @@
 import { Separator } from "@/components/ui/separator";
 import TableSemester from "./components/table-semester";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { IconInfoCircle } from "@tabler/icons-react";
 
 export default function SemesterPage() {
   return (
@@ -10,6 +12,19 @@ export default function SemesterPage() {
           <p className="text-sm text-muted-foreground">Kelola data semester</p>
         </div>
         <Separator className="my-4" />
+        <Alert>
+          <IconInfoCircle className="w-5 h-5 mr-2" />
+          <AlertTitle>Info</AlertTitle>
+          <AlertDescription>
+            Jika kamu menambahkan semester baru, maka semester yang baru
+            ditambahkan akan menjadi semester aktif dan semester sebelumnya akan
+            menjadi tidak aktif. kamu dapat mengubah semester aktif jika
+            terdapat kesalahan saat prosesi input semester. Jika kamu menghapus
+            semester aktif, maka semester yang sebelumnya akan diganti menjadi
+            semester aktif saat ini.
+          </AlertDescription>
+        </Alert>
+        <div className="my-4"></div>
         {/* table of semester that has title and status */}
         <TableSemester />
       </div>
