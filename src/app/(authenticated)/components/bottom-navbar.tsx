ef937f3e3797afee760eacf27ec8a233dd4ae0a1
akrindev <akrinmin@gmail.com>
2023-06-19 23:28:17 +0700

    fix: bottom navbar

--- src/app/(authenticated)/components/bottom-navbar.tsx
+++ src/app/(authenticated)/components/bottom-navbar.tsx
@@ -22,8 +22,14 @@ export function BottomNavbar({ className }: { className?: string }) {
   const router = useRouter();
   // change variant to default when the page is active
   // this will highlight the menu item
-  function isActive(path) {
-    return pathname.startsWith(path) ? "default" : "ghost";
+  function isActive(path, bool = false) {
+    const matched = pathname.startsWith(path);
+
+    if (!bool) {
+      return matched ? "default" : "ghost";
+    }
+
+    return matched;
   }
   return (
     <div
@@ -34,18 +40,21 @@ export function BottomNavbar({ className }: { className?: string }) {
     >
       <Separator />
       <div className="flex justify-around py-4 px-2 space-x-2">
-        {menuList.map((item) => (
-          <Button
-            key={item.name}
-            variant={isActive(item.path)}
-            size="sm"
-            className="w-full justify-center"
-            onClick={() => router.push(item.path)}
-          >
-            <item.icon className="mr-2 h-4 w-4" />
-            {item.name}
-          </Button>
-        ))}
+        {menuList.map(
+          (item) =>
+            !item.separator && (
+              <Button
+                key={item.name}
+                variant={isActive(item.path) as "default" | "ghost"}
+                size="sm"
+                className="w-full justify-center"
+                onClick={() => router.push(item.path)}
+              >
+                <item.icon className="mr-2 h-4 w-4" />
+                {isActive(item.path, true) && item.name}
+              </Button>
+            )
+        )}
       </div>
     </div>
   );
--- src/app/(authenticated)/modul/[uuid]/layout.tsx
+++ src/app/(authenticated)/modul/[uuid]/layout.tsx
@@ -51,7 +51,7 @@ export default async function Layout({
         </div>
         <Separator className="my-4" />
         <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-          <aside className="-mx-4 lg:w-1/6">
+          <aside className="mx-4 lg:-mx-4 lg:w-1/6">
             <SidebarNav items={sidebarNavItems} />
           </aside>
           <div className="flex-1">{children}</div>
