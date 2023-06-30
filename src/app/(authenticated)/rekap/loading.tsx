b556a9a65c0170be8bd1dfd5b789bb1286c5bae6
akrindev <akrinmin@gmail.com>
2023-06-30 21:59:26 +0700

    fix: base loading components

--- src/app/(authenticated)/rekap/loading.tsx
+++ src/app/(authenticated)/rekap/loading.tsx
@@ -1,18 +1,7 @@
 "use client";
 
-import { Skeleton } from "@/components/ui/skeleton";
+import BaseLoading from "@/components/base-loading";
 
 export default function Loading() {
-  return (
-    <div className="h-full flex flex-col space-y-5">
-      <div className="flex flex-col h-full">
-        <div className="flex flex-col md:flex-row justify-between">
-          <div className="mt-5 space-y-1">
-            <Skeleton className="h-14 w-24 text-2xl font-semibold tracking-tight" />
-            <Skeleton className="h-14 w-40 text-sm text-muted-foreground" />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <BaseLoading />;
 }
