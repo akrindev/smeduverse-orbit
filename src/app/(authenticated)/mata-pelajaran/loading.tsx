bf97132073f58a1fd7a6a446c3cdd54e1676065f
akrindev <akrinmin@gmail.com>
2023-06-30 21:40:44 +0700

    fix: add loading skeleton

---
+++ src/app/(authenticated)/mata-pelajaran/loading.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function SemesterPage() {
+  return (
+    <div className="h-full flex flex-col space-y-5">
+      <div className="flex flex-col h-full">
+        <div className="flex flex-col md:flex-row justify-between">
+          <div className="mt-5 space-y-1">
+            <Skeleton className="h-14 w-24 text-2xl font-semibold tracking-tight" />
+            <Skeleton className="h-14 w-40 text-sm text-muted-foreground" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/(authenticated)/rekap/page.tsx
@@ -0,0 +1,11 @@
+export default function RekapPage() {
+  return (
+    <div className="h-full flex flex-col space-y-5">
+      <div className="flex flex-col h-full">
+        <div className="flex flex-col md:flex-row justify-between">
+          <div className="mt-5 space-y-1">rekap</div>
+        </div>
+      </div>
+    </div>
+  );
+}
