d46c1dba69809a41e4c6d3d394d77a253c6c8417
akrindev <akrinmin@gmail.com>
2023-07-02 12:07:46 +0700

    fix: key on attendance

--- src/app/(authenticated)/modul/[uuid]/presensi/[presensiUuid]/components/table-presensi.tsx
+++ src/app/(authenticated)/modul/[uuid]/presensi/[presensiUuid]/components/table-presensi.tsx
@@ -27,6 +27,19 @@ export default function TablePresensi({
 }: {
   attendances: Attendance[];
 }) {
+  const generateRandomString = (length: number) => {
+    let result = "";
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+    const charactersLength = characters.length;
+
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
+  };
+
   return (
     <Table>
       <TableHeader>
@@ -50,7 +63,7 @@ export default function TablePresensi({
             <TableCell className="p-2">
               <StatusAction
                 attendance={attendance}
-                key={attendance.student_id}
+                key={attendance.student_id + generateRandomString(15)}
               />
             </TableCell>
             <TableCell className="p-2">
