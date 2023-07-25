1871f0f2cd99c7cc8cdccc41bdbf8547c88d67df
akrindev <akrinmin@gmail.com>
2023-07-25 12:31:48 +0700

    fix: small fixes

--- src/app/(authenticated)/modul/[uuid]/presensi/[presensiUuid]/components/information.tsx
+++ src/app/(authenticated)/modul/[uuid]/presensi/[presensiUuid]/components/information.tsx
@@ -49,58 +49,58 @@ export default function Information({
   }, [title, description, presenceUuid]);
 
   return (
-    <div className="grid grid-cols-12 gap-3">
-      <div className="space-y-1 col-span-12 md:col-span-6">
-        <div className="font-medium">Judul</div>
-        <div className="text-sm text-muted-foreground">{data?.title}</div>
+    <div className='grid grid-cols-12 gap-3'>
+      <div className='space-y-1 col-span-12 md:col-span-6'>
+        <div className='font-medium'>Judul</div>
+        <div className='text-sm text-muted-foreground'>{data?.title}</div>
       </div>
-      <div className="space-y-1 col-span-12 md:col-span-6">
-        <div className="font-medium">Deskripsi</div>
-        <div className="text-sm text-muted-foreground">{data?.description}</div>
+      <div className='space-y-1 col-span-12 md:col-span-6'>
+        <div className='font-medium'>Deskripsi</div>
+        <div className='text-sm text-muted-foreground'>{data?.description}</div>
       </div>
 
-      <div className="relative w-full mt-5 space-y-1 col-span-12">
+      <div className='relative w-full mt-5 space-y-1 col-span-12'>
         <ScrollArea>
-          <div className="font-medium">Rekap Kehadiran</div>
-          <div className="flex flex-wrap gap-3">
+          <div className='font-medium'>Rekap Kehadiran</div>
+          <div className='flex flex-wrap gap-3'>
             <Table>
               <TableHeader>
                 <TableRow>
-                  <TableHead>Hadir</TableHead>
-                  <TableHead>Izin</TableHead>
-                  <TableHead>Sakit</TableHead>
-                  <TableHead>Alpa</TableHead>
-                  <TableHead>Bolos</TableHead>
+                  <TableHead className='whitespace-nowrap'>Hadir</TableHead>
+                  <TableHead className='whitespace-nowrap'>Izin</TableHead>
+                  <TableHead className='whitespace-nowrap'>Sakit</TableHead>
+                  <TableHead className='whitespace-nowrap'>Alpa</TableHead>
+                  <TableHead className='whitespace-nowrap'>Bolos</TableHead>
                 </TableRow>
               </TableHeader>
               <TableBody>
                 <TableRow>
                   <TableCell>
-                    <p className="text-muted-foreground">{presence?.count_h}</p>
+                    <p className='text-muted-foreground'>{presence?.count_h}</p>
                   </TableCell>
                   <TableCell>
-                    <p className="text-muted-foreground">{presence?.count_i}</p>
+                    <p className='text-muted-foreground'>{presence?.count_i}</p>
                   </TableCell>
                   <TableCell>
-                    <p className="text-muted-foreground">{presence?.count_s}</p>
+                    <p className='text-muted-foreground'>{presence?.count_s}</p>
                   </TableCell>
                   <TableCell>
-                    <p className="text-muted-foreground">{presence?.count_a}</p>
+                    <p className='text-muted-foreground'>{presence?.count_a}</p>
                   </TableCell>
                   <TableCell>
-                    <p className="text-muted-foreground">{presence?.count_b}</p>
+                    <p className='text-muted-foreground'>{presence?.count_b}</p>
                   </TableCell>
                 </TableRow>
               </TableBody>
             </Table>
           </div>
-          <ScrollBar orientation="horizontal" />
+          <ScrollBar orientation='horizontal' />
         </ScrollArea>
       </div>
-      <div className="mt-3 flex items-center justify-end col-span-12 gap-3">
+      <div className='mt-3 flex items-center justify-end col-span-12 gap-3'>
         <DialogPresensi modulUuid={modulUuid} data={data!} />
         <Button variant={"outline"} onClick={onRefresh} disabled={isLoading}>
-          <IconReload className="w-5 h-5" />
+          <IconReload className='w-5 h-5' />
           Refresh
         </Button>
       </div>
