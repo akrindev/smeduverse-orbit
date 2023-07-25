437755022cbc01a02bc9a0019bc1f10c70d3ffbc
akrindev <akrinmin@gmail.com>
2023-07-25 10:14:52 +0700

    fix: loading skeleton

--- src/app/(authenticated)/rekap/components/class-journal.tsx
+++ src/app/(authenticated)/rekap/components/class-journal.tsx
@@ -17,9 +17,14 @@ import subDays from "date-fns/subDays";
 import { DatePickerWithRange } from "./date-picker";
 import { format } from "date-fns";
 import id from "date-fns/locale/id";
+import { is } from "date-fns/locale";
+import BaseLoading from "@/components/base-loading";
+import TableJournal from "./table-journal";
 
 export default function ClassJournal() {
   const [classJournal, setClassJournal] = useState("");
+  const [loading, setIsloading] = useState(false);
+
   const [date, setDate] = useState<DateRange | undefined>({
     from: subDays(new Date(), 1),
     to: new Date(),
@@ -32,11 +37,14 @@ export default function ClassJournal() {
   useEffect(() => {
     if (!classJournal) return;
 
+    // set loading
+    setIsloading(true);
+
     getJournal({
       rombel_id: classJournal,
       from: date?.from ? date.from : new Date(),
       to: date?.to,
-    });
+    }).finally(() => setIsloading(false));
   }, [classJournal, date]);
 
   return (
@@ -64,54 +72,7 @@ export default function ClassJournal() {
         </div>
       </div>
 
-      {journals.length > 0 ? (
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Modul</TableHead>
-              <TableHead>Judul</TableHead>
-              <TableHead>Deskripsi</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {journals.map((journal) => (
-              <TableRow key={journal.uuid}>
-                <TableCell>
-                  <div className='flex flex-col'>
-                    <div className='font-medium'>
-                      {journal.modul.mapel.nama}
-                    </div>
-                    <span className='text-muted-foreground'>
-                      {journal.modul.teacher.fullname}
-                    </span>
-                  </div>
-                </TableCell>
-                <TableCell>
-                  <div className='flex flex-col'>
-                    <div className='font-medium'>{journal.title}</div>
-                    <span className='text-muted-foreground'>
-                      {/* date */}
-                      {format(new Date(journal.created_at), "dd MMMM yyyy", {
-                        locale: id,
-                      })}
-                    </span>
-                  </div>
-                </TableCell>
-                <TableCell>{journal.description}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      ) : (
-        <div className='my-12 flex justify-center items-center h-full'>
-          <div className='flex flex-col items-center'>
-            <div className='text-2xl font-semibold'>Belum ada data</div>
-            <div className='text-md font-normal'>
-              Tidak ada data untuk ditampilkan
-            </div>
-          </div>
-        </div>
-      )}
+      {loading ? <BaseLoading /> : <TableJournal journals={journals} />}
     </div>
   );
 }
---
+++ src/app/(authenticated)/rekap/components/table-journal.tsx
@@ -0,0 +1,59 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import format from "date-fns/format";
+import id from "date-fns/locale/id";
+
+export default function TableJournal({ journals }) {
+  return journals.length > 0 ? (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Modul</TableHead>
+          <TableHead>Judul</TableHead>
+          <TableHead>Deskripsi</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {journals.map((journal) => (
+          <TableRow key={journal.uuid}>
+            <TableCell>
+              <div className='flex flex-col'>
+                <div className='font-medium'>{journal.modul.mapel.nama}</div>
+                <span className='text-muted-foreground'>
+                  {journal.modul.teacher.fullname}
+                </span>
+              </div>
+            </TableCell>
+            <TableCell>
+              <div className='flex flex-col'>
+                <div className='font-medium'>{journal.title}</div>
+                <span className='text-muted-foreground'>
+                  {/* date */}
+                  {format(new Date(journal.created_at), "dd MMMM yyyy", {
+                    locale: id,
+                  })}
+                </span>
+              </div>
+            </TableCell>
+            <TableCell>{journal.description}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  ) : (
+    <div className='my-12 flex justify-center items-center h-full'>
+      <div className='flex flex-col items-center'>
+        <div className='text-2xl font-semibold'>Belum ada data</div>
+        <div className='text-md font-normal'>
+          Tidak ada data untuk ditampilkan
+        </div>
+      </div>
+    </div>
+  );
+}
