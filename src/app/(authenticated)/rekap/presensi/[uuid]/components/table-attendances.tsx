fc35a3280e6904ffd2ffbe08c3be7b2dc769662f
Syakirin Amin <akrinmin@gmail.com>
2025-07-24 11:41:29 +0700

    refactor: streamline attendance recap page and enhance data fetching
    - Refactored the attendance recap page for improved readability and structure.
    - Enhanced data fetching logic to ensure proper handling of loading states and user authentication.
    - Updated the layout to maintain consistency across components, including the attendance and presence tables.
    - Improved the export functionality for attendance data, ensuring a smoother user experience.

--- src/app/(authenticated)/rekap/presensi/[uuid]/components/table-attendances.tsx
+++ src/app/(authenticated)/rekap/presensi/[uuid]/components/table-attendances.tsx
@@ -1,270 +1,267 @@
 "use client";
 
-import BaseLoading from "@/components/base-loading";
-import { Badge } from "@/components/ui/badge";
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { cn } from "@/lib/utils";
-import { useAttendance } from "@/store/useAttendance";
-import { Attendance, OrbitPresence } from "@/types/attendance";
-import {
-  ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  useReactTable,
+	type ColumnDef,
+	flexRender,
+	getCoreRowModel,
+	useReactTable,
 } from "@tanstack/react-table";
-import { useEffect, useMemo, useState } from "react";
-import SheetDetailAttendance from "./sheet-detail-attendance";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardDescription,
-  CardTitle,
-} from "@/components/ui/card";
-import ViewSwitcher from "@/components/ui/view-switcher";
 import { format } from "date-fns";
 import { id } from "date-fns/locale";
+import { useEffect, useMemo, useState } from "react";
+import BaseLoading from "@/components/base-loading";
+import { Badge } from "@/components/ui/badge";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
+import ViewSwitcher from "@/components/ui/view-switcher";
+import { cn } from "@/lib/utils";
+import { useAttendance } from "@/store/useAttendance";
+import type { Attendance, OrbitPresence } from "@/types/attendance";
+import SheetDetailAttendance from "./sheet-detail-attendance";
 
 interface TableAttendancesProps {
-  modulUuid: string;
+	modulUuid: string;
 }
 
 export default function TableAttendances({ modulUuid }: TableAttendancesProps) {
-  const [data, setData] = useState<Attendance[]>([]);
-  //   counted by first student orbit_presence
-  const [orbitPresence, setOrbitPresence] = useState<OrbitPresence[]>([]);
-  const [view, setView] = useState<"table" | "grid">("table");
+	const [data, setData] = useState<Attendance[]>([]);
+	//   counted by first student orbit_presence
+	const [orbitPresence, setOrbitPresence] = useState<OrbitPresence[]>([]);
+	const [view, setView] = useState<"table" | "grid">("table");
 
-  const getRecapAttendances = useAttendance(
-    (state) => state.getRecapAttendances
-  );
+	const getRecapAttendances = useAttendance(
+		(state) => state.getRecapAttendances,
+	);
 
-  const columns: ColumnDef<Attendance>[] = useMemo(
-    () => [
-      {
-        header: "No",
-        cell: (cell) => cell.row.index + 1,
-        maxSize: 50,
-      },
-      {
-        header: "Nama",
-        accessorKey: "fullname",
-        cell: (cell) => (
-          <SheetDetailAttendance attendance={cell.row.original} />
-        ),
-      },
-      ...orbitPresence.map((presence, i) => ({
-        header: () => i + 1,
-        accessorKey: presence.uuid,
+	const columns: ColumnDef<Attendance>[] = useMemo(
+		() => [
+			{
+				header: "No",
+				cell: (cell) => cell.row.index + 1,
+				maxSize: 50,
+			},
+			{
+				header: "Nama",
+				accessorKey: "fullname",
+				cell: (cell) => (
+					<SheetDetailAttendance attendance={cell.row.original} />
+				),
+			},
+			...orbitPresence.map((presence, i) => ({
+				header: () => i + 1,
+				accessorKey: presence.uuid,
 
-        //   the cell is the presence status coming from orbit_presence
-        cell: (cell) => {
-          const status =
-            cell.row.original.orbit_presence[
-              i
-            ]?.presence.status.toUpperCase() || "";
+				//   the cell is the presence status coming from orbit_presence
+				cell: (cell) => {
+					const status =
+						cell.row.original.orbit_presence[
+							i
+						]?.presence.status.toUpperCase() || "";
 
-          // colors the status
-          const colors = [
-            { status: "H", color: "text-green-500" },
-            { status: "S", color: "text-yellow-500" },
-            { status: "I", color: "text-blue-500" },
-            { status: "A", color: "text-red-500" },
-            { status: "B", color: "text-red-500" },
-          ];
+					// colors the status
+					const colors = [
+						{ status: "H", color: "text-green-500" },
+						{ status: "S", color: "text-yellow-500" },
+						{ status: "I", color: "text-blue-500" },
+						{ status: "A", color: "text-red-500" },
+						{ status: "B", color: "text-red-500" },
+					];
 
-          const matchedColor = colors.find(
-            (color) => color.status === status
-          )?.color;
+					const matchedColor = colors.find(
+						(color) => color.status === status,
+					)?.color;
 
-          return <div className={`${matchedColor} font-medium`}>{status}</div>;
-        },
-      })),
-      {
-        header: "Rekap",
-        accessorKey: "status_count",
-        cell: (cell) => {
-          return (
-            <div className='flex gap-3'>
-              {Object.entries(cell.row.original.status_count!).map(
-                ([key, value]) => (
-                  <Badge variant={"outline"} key={key}>
-                    {key.toUpperCase()}: {value}
-                  </Badge>
-                )
-              )}
-            </div>
-          );
-        },
-      },
-    ],
-    [orbitPresence]
-  );
+					return <div className={`${matchedColor} font-medium`}>{status}</div>;
+				},
+			})),
+			{
+				header: "Rekap",
+				accessorKey: "status_count",
+				cell: (cell) => {
+					return (
+						<div className="flex gap-3">
+							{Object.entries(cell.row.original.status_count!).map(
+								([key, value]) => (
+									<Badge variant={"outline"} key={key}>
+										{key.toUpperCase()}: {value}
+									</Badge>
+								),
+							)}
+						</div>
+					);
+				},
+			},
+		],
+		[orbitPresence],
+	);
 
-  const table = useReactTable({
-    data,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-  });
+	const table = useReactTable({
+		data,
+		columns,
+		getCoreRowModel: getCoreRowModel(),
+	});
 
-  useEffect(() => {
-    getRecapAttendances(modulUuid)
-      .then((res) => {
-        if (res.status === 200) {
-          // @ts-ignore
-          setData(res.data);
+	useEffect(() => {
+		getRecapAttendances(modulUuid)
+			.then((res) => {
+				if (res.status === 200) {
+					// @ts-ignore
+					setData(res.data);
 
-          //   set countedPresence by the first student orbit_presence
-          setOrbitPresence(res.data[0].orbit_presence);
-        }
-      })
-      .catch((error) => {
-        // console.log(error);
-      });
-  }, [modulUuid, getRecapAttendances, setData]);
+					//   set countedPresence by the first student orbit_presence
+					setOrbitPresence(res.data[0].orbit_presence);
+				}
+			})
+			.catch((error) => {
+				// console.log(error);
+			});
+	}, [modulUuid, getRecapAttendances]);
 
-  if (!data.length) return <BaseLoading />;
+	if (!data.length) return <BaseLoading />;
 
-  return (
-    <Card>
-      <CardHeader className="flex flex-row items-center justify-between">
-        <div>
-          <CardTitle>Daftar Kehadiran</CardTitle>
-          <CardDescription>
-            Daftar kehadiran siswa berdasarkan modul
-          </CardDescription>
-        </div>
-        <ViewSwitcher onViewChange={setView} />
-      </CardHeader>
-      <CardContent>
-        {data.length > 0 ? (
-          view === "table" ? (
-            <AttendanceDetailTable data={data} columns={columns} />
-          ) : (
-            <AttendanceDetailGrid data={data} columns={columns} />
-          )
-        ) : (
-          <div className="my-12 flex justify-center items-center h-full">
-            <div className="flex flex-col items-center">
-              <div className="text-2xl font-semibold">Belum ada data</div>
-              <div className="text-md font-normal">
-                Tidak ada data untuk ditampilkan
-              </div>
-            </div>
-          </div>
-        )}
-      </CardContent>
-    </Card>
-  );
+	return (
+		<Card>
+			<CardHeader className="flex flex-row items-center justify-between">
+				<div>
+					<CardTitle>Daftar Kehadiran</CardTitle>
+					<CardDescription>
+						Daftar kehadiran siswa berdasarkan modul
+					</CardDescription>
+				</div>
+				<ViewSwitcher onViewChange={setView} />
+			</CardHeader>
+			<CardContent>
+				{data.length > 0 ? (
+					view === "table" ? (
+						<AttendanceDetailTable data={data} columns={columns} />
+					) : (
+						<AttendanceDetailGrid data={data} columns={columns} />
+					)
+				) : (
+					<div className="my-12 flex justify-center items-center h-full">
+						<div className="flex flex-col items-center">
+							<div className="text-2xl font-semibold">Belum ada data</div>
+							<div className="text-md font-normal">
+								Tidak ada data untuk ditampilkan
+							</div>
+						</div>
+					</div>
+				)}
+			</CardContent>
+		</Card>
+	);
 }
 
 interface AttendanceDetailTableProps {
-  data: Attendance[];
-  columns: ColumnDef<Attendance>[];
+	data: Attendance[];
+	columns: ColumnDef<Attendance>[];
 }
 
 function AttendanceDetailTable({ data, columns }: AttendanceDetailTableProps) {
-  const table = useReactTable({
-    data,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-  });
-  return (
-    <div className='rounded-md border'>
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <TableHead
-                  className='p-2'
-                  key={header.id}
-                  style={{
-                    width:
-                      header.getSize() !== 150 ? header.getSize() : undefined,
-                  }}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                </TableHead>
-              ))}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows.map((row) => (
-            <TableRow key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <TableCell
-                  key={cell.id}
-                  className={cn({
-                    "p-2 whitespace-nowrap": true,
-                  })}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </TableCell>
-              ))}
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      {/* <ScrollBar orientation='horizontal' /> */}
-    </div>
-  );
+	const table = useReactTable({
+		data,
+		columns,
+		getCoreRowModel: getCoreRowModel(),
+	});
+	return (
+		<div className="rounded-md border">
+			<Table>
+				<TableHeader>
+					{table.getHeaderGroups().map((headerGroup) => (
+						<TableRow key={headerGroup.id}>
+							{headerGroup.headers.map((header) => (
+								<TableHead
+									className="p-2"
+									key={header.id}
+									style={{
+										width:
+											header.getSize() !== 150 ? header.getSize() : undefined,
+									}}
+								>
+									{header.isPlaceholder
+										? null
+										: flexRender(
+												header.column.columnDef.header,
+												header.getContext(),
+											)}
+								</TableHead>
+							))}
+						</TableRow>
+					))}
+				</TableHeader>
+				<TableBody>
+					{table.getRowModel().rows.map((row) => (
+						<TableRow key={row.id}>
+							{row.getVisibleCells().map((cell) => (
+								<TableCell
+									key={cell.id}
+									className={cn({
+										"p-2 whitespace-nowrap": true,
+									})}
+								>
+									{flexRender(cell.column.columnDef.cell, cell.getContext())}
+								</TableCell>
+							))}
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+			{/* <ScrollBar orientation='horizontal' /> */}
+		</div>
+	);
 }
 
-function AttendanceDetailGrid({
-  data,
-  columns,
-}: AttendanceDetailTableProps) {
-  return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-      {data.map((attendance: Attendance) => (
-        <Card key={attendance.student_id} className="cursor-pointer">
-          <CardHeader>
-            <CardTitle>{attendance.fullname}</CardTitle>
-            <CardDescription>{attendance.nipd}</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div className="flex flex-col">
-              {Object.entries(attendance.status_count!).map(
-                ([key, value]) => (
-                  <Badge variant={"outline"} key={key}>
-                    {key.toUpperCase()}: {value}
-                  </Badge>
-                )
-              )}
-            </div>
-            <div className="mt-2 text-sm text-muted-foreground">
-              {attendance.orbit_presence && attendance.orbit_presence.length > 0 && (
-                <p>
-                  Terakhir presensi:
-                  {format(
-                    new Date(
-                      attendance.orbit_presence?.[0].presence.created_at
-                    ),
-                    "dd MMMM yyyy",
-                    {
-                      locale: id,
-                    }
-                  )}
-                </p>
-              )}
-            </div>
-            <SheetDetailAttendance attendance={attendance} />
-          </CardContent>
-        </Card>
-      ))}
-    </div>
-  );
+function AttendanceDetailGrid({ data, columns }: AttendanceDetailTableProps) {
+	return (
+		<div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+			{data.map((attendance: Attendance) => (
+				<Card key={attendance.student_id} className="cursor-pointer">
+					<CardHeader>
+						<CardTitle>{attendance.fullname}</CardTitle>
+						<CardDescription>{attendance.nipd}</CardDescription>
+					</CardHeader>
+					<CardContent>
+						<div className="flex flex-col">
+							{Object.entries(attendance.status_count!).map(([key, value]) => (
+								<Badge variant={"outline"} key={key}>
+									{key.toUpperCase()}: {value}
+								</Badge>
+							))}
+						</div>
+						<div className="mt-2 text-sm text-muted-foreground">
+							{attendance.orbit_presence &&
+								attendance.orbit_presence.length > 0 && (
+									<p>
+										Terakhir presensi:
+										{format(
+											new Date(
+												attendance.orbit_presence?.[0].presence.created_at,
+											),
+											"dd MMMM yyyy",
+											{
+												locale: id,
+											},
+										)}
+									</p>
+								)}
+						</div>
+						<SheetDetailAttendance attendance={attendance} />
+					</CardContent>
+				</Card>
+			))}
+		</div>
+	);
 }
--- src/app/(authenticated)/rekap/presensi/[uuid]/page.tsx
+++ src/app/(authenticated)/rekap/presensi/[uuid]/page.tsx
@@ -1,111 +1,110 @@
 "use client";
 
-import { Separator } from "@/components/ui/separator";
-import { useModul } from "@/store/useModul";
-import { Modul } from "@/types/modul";
-import { useEffect, useState } from "react";
-import TableAttendances from "./components/table-attendances";
-import { Button } from "@/components/ui/button";
-import { IconDownload, IconLink } from "@tabler/icons-react";
-import { useRouter } from "next/navigation";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import TablePresences from "./components/table-presence";
-import { AxiosPromise, AxiosResponse } from "axios";
-import { isUser } from "@/lib/auth-role";
-import { useAuth } from "@/store/useAuth";
-import BaseLoading from "@/components/base-loading";
-import { api } from "@/lib/api";
+import { IconDownload } from "@tabler/icons-react";
+import type { AxiosPromise, AxiosResponse } from "axios";
 import { ArrowLeft, Loader } from "lucide-react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import BaseLoading from "@/components/base-loading";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { api } from "@/lib/api";
+import { useAuth } from "@/store/useAuth";
+import { useModul } from "@/store/useModul";
+import type { Modul } from "@/types/modul";
+import TableAttendances from "./components/table-attendances";
+import TablePresences from "./components/table-presence";
 
 interface RekapPageProps {
-  params: {
-    uuid: string;
-  };
+	params: {
+		uuid: string;
+	};
 }
 
 export default function RekapPage({ params }: RekapPageProps) {
-  const [loading, setLoading] = useState(false);
-  const [modul, fetchModul] = useModul<
-    [
-      modul: Modul | null,
-      fetchByUuid: (uuid: string) => AxiosPromise<AxiosResponse>
-    ]
-  >((state) => [state.modul, state.fetchByUuid]);
+	const [loading, setLoading] = useState(false);
+	const [modul, fetchModul] = useModul<
+		[
+			modul: Modul | null,
+			fetchByUuid: (uuid: string) => AxiosPromise<AxiosResponse>,
+		]
+	>((state) => [state.modul, state.fetchByUuid]);
 
-  const router = useRouter();
+	const router = useRouter();
 
-  // Use the custom auth store instead of next-auth
-  const { user, isAuthenticated, isLoading } = useAuth();
+	// Use the custom auth store instead of next-auth
+	const { user, isAuthenticated, isLoading } = useAuth();
 
-  useEffect(() => {
-    fetchModul(params.uuid).catch((e) => {
-      router.push("/modul");
-    });
-  }, [params.uuid]);
+	useEffect(() => {
+		fetchModul(params.uuid).catch((e) => {
+			router.push("/modul");
+		});
+	}, [params.uuid, fetchModul, router]);
 
-  if (!user || isLoading) {
-    return <BaseLoading />;
-  }
+	if (!user || isLoading || !modul) {
+		return <BaseLoading />;
+	}
 
-  // handle cetak
-  const handleExport = async () => {
-    setLoading(true);
-    // download data using axios api
-    const response = await api
-      .get(`/modul/presence/recap/export/${params.uuid}`, {
-        responseType: "blob",
-      })
-      .then((response) => {
-        const url = window.URL.createObjectURL(new Blob([response.data]));
-        const link = document.createElement("a");
-        link.href = url;
-        link.setAttribute(
-          "download",
-          `rekap presensi ${modul?.mapel.kode}-${modul?.rombel.nama}.xlsx`
-        );
-        document.body.appendChild(link);
-        link.click();
-      })
-      .finally(() => setLoading(false));
-  };
+	// handle cetak
+	const handleExport = async () => {
+		setLoading(true);
+		// download data using axios api
+		const response = await api
+			.get(`/modul/presence/recap/export/${params.uuid}`, {
+				responseType: "blob",
+			})
+			.then((response) => {
+				const url = window.URL.createObjectURL(new Blob([response.data]));
+				const link = document.createElement("a");
+				link.href = url;
+				link.setAttribute(
+					"download",
+					`rekap presensi ${modul?.mapel.kode}-${modul?.rombel.nama}.xlsx`,
+				);
+				document.body.appendChild(link);
+				link.click();
+			})
+			.finally(() => setLoading(false));
+	};
 
-  return (
-    <div className="flex flex-col space-y-5 h-full">
-      <div className="flex flex-col h-full">
-        <div className="flex md:flex-row flex-col justify-between">
-          <div className="space-y-1 mt-5">
-            <h2 className="font-semibold text-2xl tracking-tight">
-              Rekap Presensi
-            </h2>
-            <p className="text-muted-foreground text-sm">
-              Rekap presensi pada modul {modul?.mapel.nama} <br />
-              {modul?.rombel.nama} - {modul?.teacher.fullname}
-            </p>
-          </div>
-          <div className="flex gap-3 mt-5 md:mt-0">
-            <Button variant="outline" onClick={handleExport} disabled={loading}>
-              {loading ? (
-                <Loader className="w-5 h-5" />
-              ) : (
-                <div className="flex items-center gap-2">
-                  Unduh
-                  <IconDownload className="w-4 h-4" />
-                </div>
-              )}
-            </Button>
-            <div className="flex mb-2">
-              <Link href={`/modul/${params.uuid}`}>
-                <Button
-                  variant="outline"
-                  className="flex items-center gap-2 w-fit"
-                >
-                  <ArrowLeft className="w-4 h-4" />
-                  Kembali
-                </Button>
-              </Link>
-            </div>
-            {/* {isUser(user, modul?.teacher_id!) && (
+	return (
+		<div className="flex flex-col space-y-5 h-full">
+			<div className="flex flex-col h-full">
+				<div className="flex md:flex-row flex-col justify-between">
+					<div className="space-y-1 mt-5">
+						<h2 className="font-semibold text-2xl tracking-tight">
+							Rekap Presensi
+						</h2>
+						<p className="text-muted-foreground text-sm">
+							Rekap presensi pada modul {modul?.mapel.nama} <br />
+							{modul?.rombel.nama} - {modul?.teacher.fullname}
+						</p>
+					</div>
+					<div className="flex gap-3 mt-5 md:mt-0">
+						<Button variant="outline" onClick={handleExport} disabled={loading}>
+							{loading ? (
+								<Loader className="w-5 h-5" />
+							) : (
+								<div className="flex items-center gap-2">
+									Unduh
+									<IconDownload className="w-4 h-4" />
+								</div>
+							)}
+						</Button>
+						<div className="flex mb-2">
+							<Link href={`/modul/${params.uuid}`}>
+								<Button
+									variant="outline"
+									className="flex items-center gap-2 w-fit"
+								>
+									<ArrowLeft className="w-4 h-4" />
+									Kembali
+								</Button>
+							</Link>
+						</div>
+						{/* {isUser(user, modul?.teacher_id!) && (
               <Link href={`/modul/${params.uuid}`}>
                 <Button variant='default'>
                   Lihat
@@ -113,24 +112,24 @@ export default function RekapPage({ params }: RekapPageProps) {
                 </Button>
               </Link>
             )} */}
-          </div>
-        </div>
-        <Separator className="my-4" />
-        {/* table */}
-        <Tabs defaultValue="rekap">
-          <TabsList className="grid grid-cols-2 max-w-md">
-            <TabsTrigger value="rekap">Kehadiran</TabsTrigger>
-            <TabsTrigger value="presensi">Presensi</TabsTrigger>
-          </TabsList>
-          <TabsContent value="rekap">
-            <TableAttendances modulUuid={params.uuid} />
-          </TabsContent>
-          <TabsContent value="presensi">
-            <TablePresences modulUuid={params.uuid} />
-          </TabsContent>
-        </Tabs>
-        {/* end: table */}
-      </div>
-    </div>
-  );
+					</div>
+				</div>
+				<Separator className="my-4" />
+				{/* table */}
+				<Tabs defaultValue="rekap">
+					<TabsList className="grid grid-cols-2 max-w-md">
+						<TabsTrigger value="rekap">Kehadiran</TabsTrigger>
+						<TabsTrigger value="presensi">Presensi</TabsTrigger>
+					</TabsList>
+					<TabsContent value="rekap">
+						<TableAttendances modulUuid={params.uuid} />
+					</TabsContent>
+					<TabsContent value="presensi">
+						<TablePresences modulUuid={params.uuid} />
+					</TabsContent>
+				</Tabs>
+				{/* end: table */}
+			</div>
+		</div>
+	);
 }
